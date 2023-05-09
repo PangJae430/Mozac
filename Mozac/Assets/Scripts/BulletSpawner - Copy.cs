db69a87506c9e3028bb686c1852eba9b03f42f0e
PangJae430 <leekj0430@naver.com>
2023-05-09 19:41:27 +0900

    2023-05-09/19:41

---
+++ Mozac/Assets/Scripts/BulletSpawner - Copy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 1f;
+
+    private float spawnRate;
+    private float timeAfterSpawn;
+    float rotSpeed = 100f;
+
+    void Start()
+    {
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+
+        if (timeAfterSpawn >= spawnRate)
+        {
+            timeAfterSpawn = 0f;
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+            spawnRate = Random.Range(spawnRateMin, spawnRateMin);
+        }
+        
+        transform.Rotate(new Vector3(0, rotSpeed * Time.deltaTime, 0));
+    }
+}
