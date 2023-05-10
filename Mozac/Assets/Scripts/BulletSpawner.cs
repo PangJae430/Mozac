43be6d5ebd33c874df4fdaa211f03b760135dfe3
PangJae430 <leekj0430@naver.com>
2023-05-10 19:27:57 +0900

    2023-05-10/19:27

--- Mozac/Assets/Scripts/BulletSpawner.cs
+++ Mozac/Assets/Scripts/BulletSpawner.cs
@@ -4,34 +4,36 @@ using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
-    public GameObject bulletPrefab;
-    public float spawnRateMin = 0.5f;
-    public float spawnRateMax = 2f;
+    public GameObject bulletPrefab; //  생성할 BulletSpawner 원본 프리팹
+    private float spawnRateMin = 0.2f; // 최소 생성 주기
+    private float spawnRateMax = 1f; // 최대 생성 주기
 
-    private float spawnRate;
-    private float timeAfterSpawn;
-    float rotSpeed = 100f;
+    private float spawnRate; // 생성 주기
+    private float timeAfterSpawn; // 최근 생성 시점에서 지난 시간
+    float rotSpeed = 100f; // BulletSpawner 회전 각도
 
     void Start()
     {
-        timeAfterSpawn = 0f;
-        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        timeAfterSpawn = 0f; // 최근 생성 이후의 누적시간을 0으로 초기화
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax); // 탄알 생성 간격을 spawnRateMin과 spawnRateMax 사이에서 랜덤 지정
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        //timeAfterSpawn 갱신
         timeAfterSpawn += Time.deltaTime;
 
         if (timeAfterSpawn >= spawnRate)
         {
+            // 누적된 시간 리셋
             timeAfterSpawn = 0f;
-
+            // transform.position 위치와 transform.rotation 회전으로 생성
             GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
-
+            // 다음 총알 생성 간격을 spawnRateMin, spawnRateMin 사이에서 지정
             spawnRate = Random.Range(spawnRateMin, spawnRateMin);
         }
-        
+        // BulletSpawner가 Y축 기준으로 1초에 100도회전
         transform.Rotate(new Vector3(0, rotSpeed * Time.deltaTime, 0));
     }
 }
--- Mozac/Assets/Scripts/GameManager.cs
+++ Mozac/Assets/Scripts/GameManager.cs
@@ -1,51 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI; // UI 관련 라이브러리
+using UnityEngine.SceneManagement; // 씬 관리 관련 라이브러리
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject gameoverText;
-    public Text timeText;
-    public Text recordText;
+    public GameObject gameoverText; // 게임오버 시 활성화할 텍스트 게임 오브젝트
+    public Text timeText; // 생존 시간을 표시할 텍스트 컴포넌트
+    public Text recordText; // 최고 기록을 표시할 텍스트 컴포넌트
 
-    private float surviveTime;
-    private bool isGameover;
+    private float surviveTime; // 생존 시간
+    private bool isGameover; // 게임 오버 상태
 
     void Start()
     {
+        // 생존 시간과 게임오버 상태 초기화
         surviveTime = 0;
         isGameover = false;
     }
 
     void Update()
     {
+        // 게임오버가 아닌 동안
         if(!isGameover)
         {
+            // 생존 시간 갱신
             surviveTime += Time.deltaTime;
+            // 갱신한 생존 시간을 timeText 텍스트 컴포넌트를 이용해 표시
             timeText.text = "Time: " + (int)surviveTime;
         }
         else
         {
+            // 게임오버인 상태에서 R키를 누른 경우
             if(Input.GetKeyDown(KeyCode.R))
             {
+                // SmapleScene 씬을 로드
                 SceneManager.LoadScene("SampleScene");
             }
         }
     }
+
+    // 현재 게임을 게임오버 상태로 변경하는 메서드
     public void EndGame()
     {
-        isGameover = true;
-        gameoverText.SetActive(true);
+        isGameover = true; // 현재 상태를 게임오버 상태로 전환
+        gameoverText.SetActive(true); // 게임오버 텍스트 게임 오브젝트를 활성화
 
-        float bestTime = PlayerPrefs.GetFloat("BestTime");
+        float bestTime = PlayerPrefs.GetFloat("BestTime"); // BestTime 키로 저장된 이전까지의 최고 기록 가져오기
 
+        // 이전까지의 최고 기록보다 현재 생존 시간이 더 크다면
         if(surviveTime>bestTime)
         {
-            bestTime = surviveTime;
-            PlayerPrefs.SetFloat("BestTime", bestTime);
+            bestTime = surviveTime; // 최고 기록 값을 현재 생존 시간 값으로 변경
+            PlayerPrefs.SetFloat("BestTime", bestTime); // 변경된 최고 기록을 BestTime 키로 저장
         }
+        // 최고 기록을 recordText 텍스트 컴포넌트를 이용해 표시
         recordText.text = "Best Time: " + (int)bestTime;
     }
 }
